53d1cf62f728575580200d8b6a7403e447d014d2
Vicky <vickyyukaiyin@hotmail.com>
2017-06-02 22:29:40 -0700

    first commit

---
+++ home.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html>
+<title>Vicky Kaiyin Yu's Website</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+</style>
+<body class="w3-light-grey">
+
+<!-- Page Container -->
+<div class="w3-content w3-margin-top" style="max-width:1400px;">
+
+  <!-- The Grid -->
+  <div class="w3-row-padding">
+  
+    <!-- Left Column -->
+    <div class="w3-third">
+    
+      <div class="w3-white w3-text-grey w3-card-4">
+        <div class="w3-display-container">
+          <img src="images/services/vicky.png" style="width:100%" alt="Avatar">
+        </div>
+        <div class="w3-container">
+          <h1> Vicky Kaiyin Yu </h1>
+          <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i>Computer Science & Management Information Systems</p>
+          <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i>San Jose, CA</p>
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>vickykyyu@gmail.com</p>
+         
+          <hr>
+
+          <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>Skills</b></p>
+          <p>Java</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:90%">90%</div>
+          </div>
+          <p>Visual Basic</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:90%">
+              <div class="w3-center w3-text-white">90%</div>
+            </div>
+          </div>
+          <p>SQL</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:90%">90%</div>
+          </div>
+          <p>SalesForce</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:90%">90%</div>
+          </div>
+          <p>Project Management</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:90%">90%</div>
+          </div>
+          <p>Presentation</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:70%">70%</div>
+          </div>
+
+          <br>
+
+          <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>Languages</b></p>
+          <p>English</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:95%"></div>
+          </div>
+          <p>Chinese Cantonese</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:100%"></div>
+          </div>
+          <p>Chinese Mandarin</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:95%"></div>
+          </div>
+          <br>
+        </div>
+      </div><br>
+
+    <!-- End Left Column -->
+    </div>
+
+    <!-- Right Column -->
+    <div class="w3-twothird">
+    
+        <div class="w3-container w3-card-2 w3-white w3-margin-bottom">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-institution fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Honors & Awards</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Gary J. Sbona Honors Program</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>04/2012 - 07/2014</h6>
+          <p>San Jose State University, Fundamentals of Management Information Systems</p>
+          <li> Identify and solve students' codes problems from their course project </li>
+          <li> Give feedback on students' group projects and help them improve application design, including GUI </li>
+        </div>
+    </div>
+
+      <div class="w3-container w3-card-2 w3-white w3-margin-bottom">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-bar-chart fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Projects</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Instructional Student Assistant | SalesForce, VB.Net, SQL</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>04/2012 - 07/2014</h6>
+          <p>San Jose State University, Fundamentals of Management Information Systems</p>
+          <li> Identify and solve students' codes problems from their course project </li>
+          <li> Give feedback on students' group projects and help them improve application design, including GUI </li>
+        </div>
+    </div>
+
+
+
+      <div class="w3-container w3-card-2 w3-white w3-margin-bottom">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Work Experience</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Instructional Student Assistant | SalesForce, VB.Net, SQL</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>08/2015 - <span class="w3-tag w3-teal w3-round">Present</span></h6>
+          <p>San Jose State University, Fundamentals of Management Information Systems</p>
+          <li> Identify and solve students' codes problems from their course project </li>
+          <li> Give feedback on students' group projects and help them improve application design, including GUI </li>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Student Assistant, International Recruitment and Partner</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>08/2014 - 02/2017</h6>
+          <p>San Jose State University</p>
+          <li> Implemented new CRM systems, resulting in increasing responding rates and process automation </li>
+          <li> Created documentations and testing for the CRM system</li>
+          <li> Checked and updated department's web page</li>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Tutor</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jun 2010 - Mar 2012</h6>
+          <p>Jack Holland Student Success Center, San Jose State University</p>
+          <li>Fundamentals of Management Information Systems, System Analysis and Design, and Quantitative Business Analysis </li>
+        
+          <br>
+        </div>
+
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Student Assistant</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>04/2012 - 07/2014</h6>
+          <p>International Education Center, Pleasant Hill, CA</p>
+          <li> Defined the business requirements, business work flow, designing and implementing a new database </li>
+          <li> Tested, improved and maintained with programmers after the database had been integrated</li>
+          <li> Tested school website regularly and provided basic IT support to staff and students</li>
+        
+          <br>
+        </div>
+
+      </div>
+
+      <div class="w3-container w3-card-2 w3-white">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-mortar-board fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Education</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>San Jose State University</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Expected Graduation Date: Dec 2017</h6>
+          <li>BS in Computer Science</li>
+          <li>BS in Business Administrations, concentration in Management Information Systems</li>
+          <li>Minor in Mathematics</li>
+          <li>Certificate -- Cyber Security</li>
+          <hr>
+        </div>
+        
+      </div>
+
+    <!-- End Right Column -->
+    </div>
+    
+  <!-- End Grid -->
+  </div>
+  
+  <!-- End Page Container -->
+</div>
+
+<footer class="w3-container w3-teal w3-center w3-margin-top">
+  <p>Find me on social media.</p>
+    <a href="https://www.linkedin.com/in/kaiyinyu" target="_blank"><i class="fa fa-linkedin w3-hover-opacity"></i></a>
+  
+  <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+</footer>
+
+</body>
+</html>
+
